14aed550e0868374ed9146e0dd1d9aeccc96841b
ZaharKaras <93844318+ZaharKaras@users.noreply.github.com>
2022-08-16 02:08:56 +0300

    Simple Camera

---
+++ Unit/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public float minY;
+    public float maxY;
+    public Vector2 panLimint;
+
+    public float scrollSpeed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey("a") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey("d") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * Time.deltaTime * 100f;
+
+        pos.x = Mathf.Clamp(pos.x, -panLimint.x, panLimint.x);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp (pos.z, -panLimint.y, panLimint.y);
+
+        transform.position = pos;
+        transform.rotation = Quaternion.Euler(pos.y * 3, 0f, 0f);
+    }
+}
